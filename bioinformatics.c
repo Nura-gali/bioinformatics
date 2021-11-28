658ea8bdac81b9c048360ad679c06d310cf63d32
Nura Gali <95077760+Nura-gali@users.noreply.github.com>
2021-11-28 17:26:55 +0100

    Create bioinformatics.c

---
+++ bioinformatics.c
@@ -0,0 +1,20 @@
+
+int main()
+{
+    /*Programme to print out my details
+    Written by:
+        NURA GALI
+        BASUG/UG/BHS/ANT/ANA/18/007
+
+    */
+
+
+
+    printf("BIOINFORMATICS");
+    printf("NAME: NURA GALI\n");
+    printf("EMAIL: nuragali6154@gmail.com\n");
+    printf("SLACK USERNAME: @Nura Gali\n");
+    printf("GITHUB USERNAME: nura-gali\n");
+    printf("AREA OF INTEREST: SYSTEM BIOLOGY\n");
+    return 0;
+}
